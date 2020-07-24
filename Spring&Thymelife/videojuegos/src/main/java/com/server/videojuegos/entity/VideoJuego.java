0d88b064b5ccceb20c073b82993b2518095eeeae
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-07-24 18:33:21 -0500

    application.properties

--- Spring&Thymelife/videojuegos/src/main/java/com/server/videojuegos/entity/VideoJuego.java
+++ Spring&Thymelife/videojuegos/src/main/java/com/server/videojuegos/entity/VideoJuego.java
@@ -1,7 +1,16 @@
 package com.server.videojuegos.entity;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.*;
+
+@Entity
 public class VideoJuego {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String nombre;
     private String descripcion;
     private String imageUrl;
