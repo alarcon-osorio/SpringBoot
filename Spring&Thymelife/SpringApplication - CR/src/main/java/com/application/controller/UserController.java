a905e3707a2bb098dc11b030644a173a3df25aed
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-08-28 15:13:20 -0500

    Controller y HTML. Corregido

--- Spring&Thymelife/SpringApplication - CR/src/main/java/com/application/controller/UserController.java
+++ Spring&Thymelife/SpringApplication - CR/src/main/java/com/application/controller/UserController.java
@@ -1,36 +1,18 @@
 package com.application.controller;
 
-
-import com.application.entity.User;
-import com.application.repository.RoleRepository;
-import com.application.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    RoleRepository roleRepository;
-
-    @GetMapping({"/", "/login"})
+    @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @RequestMapping(value = "/userForm", method = RequestMethod.POST)
-    public String getUserForm(Model model) {
-        model.addAttribute("userForm", new User());
-        model.addAttribute("roles",roleRepository.findAll());
-        model.addAttribute("userList", userService.getAllUsers());
-        model.addAttribute("formTab","active");
+    @GetMapping("/userForm")
+    public String getUserForm() {
         return "user-form/user-view";
     }
 
