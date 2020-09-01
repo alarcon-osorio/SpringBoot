7f532c15a75c8782b6fc87ef76a77473ea4d8617
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-08-31 20:06:56 -0500

    Creando Entidad Estudiantes. Close #5

--- SpringApiRest(Elivar)/backend/src/main/java/com/server/backend/BackendApplication.java
+++ SpringApiRest(Elivar)/backend/src/main/java/com/server/backend/BackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication //(exclude = DataSourceAutoConfiguration.class) -- Se usa para excluir errores de JPA
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ SpringApiRest(Elivar)/backend/src/main/java/com/server/backend/Entity/Estudiante.java
@@ -0,0 +1,70 @@
+package com.server.backend.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Estudiantes")
+public class Estudiante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Double nota;
+    private String email;
+
+    public Estudiante() { //Constructor
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "id=" + id +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", nota=" + nota +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
